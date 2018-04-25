a64c7a52cc57635cc96fe672f499ae560bd5e1e8
Nicolas Aoki <nicolasaoki@alunos.utfpr.edu.br>
2018-04-25 15:07:25 -0300

    att downloads e index

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	<title>streptoRNA</title>
 	<!-- Google font -->
 	<link href="https://fonts.googleapis.com/css?family=Lato:700%7CMontserrat:400,600" rel="stylesheet">
