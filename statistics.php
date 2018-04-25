a8a4fc217249dff479c51183af5dd23157e469bd
Nicolas Aoki <nicolasaoki@alunos.utfpr.edu.br>
2018-04-25 16:11:10 -0300

    graphics atualizado

---
+++ statistics.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>StreptoRNA</title>
+                <script type="text/javascript" src="js/jquery.min.js"></script>
+                <script type="text/javascript" src="js/bootstrap.min.js"></script>
+                <script type="text/javascript" src="js/main.js"></script>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:700%7CMontserrat:400,600" rel="stylesheet">
+
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css"/>
+
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+
+
+        <link type="text/css" rel="stylesheet" href="css/style.css"/>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+    </head>
+    <body>
+        <?php
+            include('cabecalho.php');
+        ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h1>Statistics</h1>
+                </div>
+            </div>
+        </div>
+        <div class="spaceTop"></div>
+    </body>
+</html>
